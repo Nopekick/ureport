6b60444a68e3ce75267cbf4eea9b0507c568f7e1
Thomas Kamm <33606565+Nopekick@users.noreply.github.com>
2020-06-12 10:25:47 -0700

    Update misc.js

--- project-backend/helpers/misc.js
+++ project-backend/helpers/misc.js
@@ -1,90 +1,6 @@
 const crypto = require("crypto")
 const db = require("../models")
 
-// let teachers = ["Andrea Aquino",
-// "Claire Ballard",
-// "Jill Buensuceso",
-// "Esther Chen",
-// "Tom Clark",
-// "Karen Crump",
-// "Jesus Del Real",
-// "Jaz Dhillon",
-// "Catherine Dietrich",
-// "Kimberly Gavin",
-// "Steve Guevara",
-// "Leo Salcedo",
-// "Alan Khan",
-// "Eric Sato",
-// "Christopher Lucas",
-// "Jean Mastrogiacomo",
-// "Nico Mendoza",
-// "Kristin Moore",
-// "Maria Murillo",
-// "Vaishali Patil",
-// "Liz Pettit",
-// "Jessica Reid",
-// "Katherine Poltorak",
-// "Richard Prizznick",
-// "Jemal Ramirez",
-// "Laura Gordon Reska",
-// "Ariana Rodriguez",
-// "Loren Schwinge",
-// "Nicole Sebek",
-// "Sandra Trotch",
-// "Aimee Verapinto",
-// "Jessica Wall",
-// "Anne Wustrow"]
-//
-// let teacheremails = ["aaquino@upatoday.com",
-// "cballard@upatoday.com",
-// "jbuensuceso@upatoday.com",
-// "echen@upatoday.com",
-// "tclark@upatoday.com",
-// "kcrump@upatoday.com",
-// "jdelreal@upatoday.com",
-// "jdhillon@upatoday.com",
-// "cdietrich@upatoday.com",
-// "kgavin@upatoday.com",
-// "sguevara@upatoday.com",
-// "lsalcedo@upatoday.com",
-// "akhan@upatoday.com",
-// "esato@upatoday.com",
-// "clucas@upatoday.com",
-// "jmastrogiacomo@upatoday.com",
-// "nmendoza@upatoday.com",
-// "kmoore@upatoday.com",
-// "mmurillo@upatoday.com",
-// "vpatil@upatoday.com",
-// "lpettit@upatoday.com",
-// "jreid@upatoday.com",
-// "kpoltorak@upatoday.com",
-// "rprizznick@upatoday.com",
-// "jramirez@upatoday.com",
-// "lgordonreska@upatoday.com",
-// "arodriguez@upatoday.com",
-// "lschwinge@upatoday.com",
-// "nsebek@upatoday.com",
-// "strotch@upatoday.com",
-// "averapinto@upatoday.com",
-// "jwall@upatoday.com",
-// "awustrow@upatoday.com"]
-// db.School.findOne({name: "University Preparatory Academy"}).then((upa)=>{
-//   console.log(upa)
-//   for(let i = 0; i < teachers.length; i++){
-//     let obj = {
-//       name: teachers[i],
-//       email: teacheremails[i],
-//       school: upa._id
-//     }
-//     db.Teacher.create(obj).then((teacher)=>{
-//       upa.teachers.push(teacher)
-//
-//     })
-//
-//   }
-//   upa.save()
-// })
-
 exports.createHash = function(email){
   email = email + "secret393939"
   return crypto.createHash('sha1').update(`${email}`).digest('hex');
@@ -99,7 +15,6 @@ exports.getTeachers = function(req, res, next){
   //     next({message: err, status: 400})
   // })
 
-  // This chunk below was commented out before hardcoding in Ms. nsebek
   // db.Teacher.find({school: req.params.id})
   // .then((teachers)=>{
   //   res.status(200).json(teachers)
@@ -107,7 +22,7 @@ exports.getTeachers = function(req, res, next){
   //     next({message: err, status: 400})
   // })
 
-  db.Teacher.find({email: 'nsebek@upatoday.com'})
+  db.Teacher.find({email: ''})
   .then((teacher) => {
     res.status(200).json(teacher)
   }).catch((err)=>{
